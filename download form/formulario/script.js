57d8346e683437570452e88531c1c2c8c8749d5e
Douglas Pádua <douglashenriq@live.com>
2021-08-24 21:32:20 -0300

    commit full form

---
+++ download form/formulario/script.js
@@ -0,0 +1,9 @@
+let nom = document.querySelector('input#nome')
+let sob = document.querySelector('input#sobrenome')
+let em = document.querySelector('input#email')
+
+if (nom.length == 0) {
+    window.alert('preencha o espaço "nome"')
+} else {
+    alert('tudo ok')
+}
